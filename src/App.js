9785423cc5c2823bfba011e8f697e8eb2793720d
Chorro Michael <michaelchorro.dev@gmail.com>
2022-09-06 14:05:26 +0200

    création des pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import { StockDetailPage } from "./pages/StockDetailPage";
+import { StockOverviewPage } from "./pages/StockOverviewPage";
+
 
 function App() {
   return (
     <div className="App">
+      <StockDetailPage />
+      <StockOverviewPage />
       <h2 className='text-danger mt-5'>hello world</h2>
     </div>
   );
